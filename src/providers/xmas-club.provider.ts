46f9f82960b08cdcedb5d1c8f35801cc7e5ff8f7
Bobby Thompson <bobby.m.thompson@gmail.com>
2017-09-30 08:17:49 -0400

    Patch for missing bold on home team for scorecard

--- src/providers/xmas-club.provider.ts
+++ src/providers/xmas-club.provider.ts
@@ -111,7 +111,7 @@ export class XmasClubDataProvider {
         let result = this.calculatePickResult(theWeek, pick, gameResults);
 
         pick.homeTeam = result.homeTeam;
-        
+
         if (result.correct) {
           scorecard.score++;
         }
@@ -176,70 +176,67 @@ export class XmasClubDataProvider {
       homeTeam: ''
     };
 
-    if (new Date() >= new Date(week.dueDate)) {
+    let game = _.find(gameResults, (game) => {
+      return (game.team1.name.toLowerCase() == pick.team1.toLowerCase()) && (game.team2.name.toLowerCase() == pick.team2.toLowerCase())
+    });
 
-      let game = _.find(gameResults, (game) => {
-        return (game.team1.name.toLowerCase() == pick.team1.toLowerCase()) && (game.team2.name.toLowerCase() == pick.team2.toLowerCase())
-      });
+    if (!game) {
+      console.log(`Unable to find a game for teams. Team1: '${pick.team1}' - Team2: '${pick.team2}' - Spread: '${pick.spread}' - Type: '${pick.pickType}'`);
+    } else {
 
-      if (!game) {
-        console.log(`Unable to find a game for teams. Team1: '${pick.team1}' - Team2: '${pick.team2}' - Spread: '${pick.spread}' - Type: '${pick.pickType}'`);
-      } else {
+      /* Set the home team on this pick. */
+      result.homeTeam = game.homeTeam;
 
-        /* Set the home team on this pick. */
-        result.homeTeam = game.homeTeam;
+      if (game.status == "Complete") {
 
-        if (game.status == "Complete") {
+        result.complete = true;
 
-          result.complete = true;
+        let spread = parseFloat(pick.spread);
+        if (isNaN(spread)) {
+          /* The spread is a 'PICK' */
+          spread = 0;
+        }
 
-          let spread = parseFloat(pick.spread);
-          if (isNaN(spread)) {
-            /* The spread is a 'PICK' */
-            spread = 0;
+        if (pick.isOverUnder) {
+
+          let totalScore = game.team1.score + game.team2.score;
+
+          if (spread) {
+
+            if (pick.selectedPick == "Team1") {
+              if (totalScore >= spread) {
+                result.correct = true;
+              }
+            } else if (pick.selectedPick == "Team2") {
+              if (totalScore <= spread) {
+                result.correct = true;
+              }
+            }
           }
 
-          if (pick.isOverUnder) {
+        }
+        else {
 
-            let totalScore = game.team1.score + game.team2.score;
+          if (pick.selectedPick == "Team1") {
 
-            if (spread) {
+            if (game.winner == "Team1") {
 
-              if (pick.selectedPick == "Team1") {
-                if (totalScore >= spread) {
-                  result.correct = true;
-                }
-              } else if (pick.selectedPick == "Team2") {
-                if (totalScore <= spread) {
-                  result.correct = true;
-                }
+              if (game.team1.score >= (game.team2.score + spread)) {
+                result.correct = true;
               }
             }
 
-          }
-          else {
+          } else if (pick.selectedPick == "Team2") {
 
-            if (pick.selectedPick == "Team1") {
+            if (game.winner == "Team2") {
+              /* The underdog was picked and they won. */
+              result.correct = true;
+            }
+            else {
 
-              if (game.winner == "Team1") {
-
-                if (game.team1.score >= (game.team2.score + spread)) {
-                  result.correct = true;
-                }
-              }
-
-            } else if (pick.selectedPick == "Team2") {
-
-              if (game.winner == "Team2") {
-                /* The underdog was picked and they won. */
+              /* The underdog lost, check the spread. */
+              if ((game.team2.score + spread) >= game.team1.score) {
                 result.correct = true;
-              }
-              else {
-
-                /* The underdog lost, check the spread. */
-                if ((game.team2.score + spread) >= game.team1.score) {
-                  result.correct = true;
-                }
               }
             }
           }
--- www/build/main.js
+++ www/build/main.js
@@ -489,56 +489,54 @@ let XmasClubDataProvider = class XmasClubDataProvider {
             correct: false,
             homeTeam: ''
         };
-        if (new Date() >= new Date(week.dueDate)) {
-            let game = _.find(gameResults, (game) => {
-                return (game.team1.name.toLowerCase() == pick.team1.toLowerCase()) && (game.team2.name.toLowerCase() == pick.team2.toLowerCase());
-            });
-            if (!game) {
-                console.log(`Unable to find a game for teams. Team1: '${pick.team1}' - Team2: '${pick.team2}' - Spread: '${pick.spread}' - Type: '${pick.pickType}'`);
-            }
-            else {
-                /* Set the home team on this pick. */
-                result.homeTeam = game.homeTeam;
-                if (game.status == "Complete") {
-                    result.complete = true;
-                    let spread = parseFloat(pick.spread);
-                    if (isNaN(spread)) {
-                        /* The spread is a 'PICK' */
-                        spread = 0;
-                    }
-                    if (pick.isOverUnder) {
-                        let totalScore = game.team1.score + game.team2.score;
-                        if (spread) {
-                            if (pick.selectedPick == "Team1") {
-                                if (totalScore >= spread) {
-                                    result.correct = true;
-                                }
-                            }
-                            else if (pick.selectedPick == "Team2") {
-                                if (totalScore <= spread) {
-                                    result.correct = true;
-                                }
-                            }
-                        }
-                    }
-                    else {
+        let game = _.find(gameResults, (game) => {
+            return (game.team1.name.toLowerCase() == pick.team1.toLowerCase()) && (game.team2.name.toLowerCase() == pick.team2.toLowerCase());
+        });
+        if (!game) {
+            console.log(`Unable to find a game for teams. Team1: '${pick.team1}' - Team2: '${pick.team2}' - Spread: '${pick.spread}' - Type: '${pick.pickType}'`);
+        }
+        else {
+            /* Set the home team on this pick. */
+            result.homeTeam = game.homeTeam;
+            if (game.status == "Complete") {
+                result.complete = true;
+                let spread = parseFloat(pick.spread);
+                if (isNaN(spread)) {
+                    /* The spread is a 'PICK' */
+                    spread = 0;
+                }
+                if (pick.isOverUnder) {
+                    let totalScore = game.team1.score + game.team2.score;
+                    if (spread) {
                         if (pick.selectedPick == "Team1") {
-                            if (game.winner == "Team1") {
-                                if (game.team1.score >= (game.team2.score + spread)) {
-                                    result.correct = true;
-                                }
+                            if (totalScore >= spread) {
+                                result.correct = true;
                             }
                         }
                         else if (pick.selectedPick == "Team2") {
-                            if (game.winner == "Team2") {
-                                /* The underdog was picked and they won. */
+                            if (totalScore <= spread) {
                                 result.correct = true;
                             }
-                            else {
-                                /* The underdog lost, check the spread. */
-                                if ((game.team2.score + spread) >= game.team1.score) {
-                                    result.correct = true;
-                                }
+                        }
+                    }
+                }
+                else {
+                    if (pick.selectedPick == "Team1") {
+                        if (game.winner == "Team1") {
+                            if (game.team1.score >= (game.team2.score + spread)) {
+                                result.correct = true;
+                            }
+                        }
+                    }
+                    else if (pick.selectedPick == "Team2") {
+                        if (game.winner == "Team2") {
+                            /* The underdog was picked and they won. */
+                            result.correct = true;
+                        }
+                        else {
+                            /* The underdog lost, check the spread. */
+                            if ((game.team2.score + spread) >= game.team1.score) {
+                                result.correct = true;
                             }
                         }
                     }
@@ -556,10 +554,12 @@ let XmasClubDataProvider = class XmasClubDataProvider {
 };
 XmasClubDataProvider = __decorate([
     core_1.Injectable(),
-    __metadata("design:paramtypes", [typeof (_a = typeof http_1.Http !== "undefined" && http_1.Http) === "function" && _a || Object, typeof (_b = typeof database_1.AngularFireDatabase !== "undefined" && database_1.AngularFireDatabase) === "function" && _b || Object, typeof (_c = typeof auth_1.AngularFireAuth !== "undefined" && auth_1.AngularFireAuth) === "function" && _c || Object, typeof (_d = typeof scorecards_provider_1.ScorecardsProvider !== "undefined" && scorecards_provider_1.ScorecardsProvider) === "function" && _d || Object])
+    __metadata("design:paramtypes", [http_1.Http,
+        database_1.AngularFireDatabase,
+        auth_1.AngularFireAuth,
+        scorecards_provider_1.ScorecardsProvider])
 ], XmasClubDataProvider);
 exports.XmasClubDataProvider = XmasClubDataProvider;
-var _a, _b, _c, _d;
 //# sourceMappingURL=xmas-club.provider.js.map
 
 /***/ }),
